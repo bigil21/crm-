a3b3548f28ae9c90a2fbf0be45c6bc69c8145357
bigil21 <gil@coastalcrestroofing.com>
2026-07-14 19:08:13 -0400

    Cache profit role diagnostic page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "roofline-crm-v48";
+const CACHE_NAME = "roofline-crm-v49";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -7,6 +7,7 @@ const APP_SHELL = [
   "./reset-session.html",
   "./diagnostics.html",
   "./role-diagnostics.html",
+  "./profit-role-test.html",
   "./auth-config.js",
   "./auth-config.js?v=43",
   "./auth-config.js?v=46",
